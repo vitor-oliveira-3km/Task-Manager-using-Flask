e19330d20485e9e070e1fead7a2c784594568bf9
Vitor Oliveira <@123>
2026-06-09 08:46:04 -0400

    Primeira versão do projeto

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -2,18 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+import os
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 bcrypt = Bcrypt(app)
 
-# Always put Routes at end
 from todo_project import routes
